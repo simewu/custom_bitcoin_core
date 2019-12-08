ea06ea14433181d6778a891e08648dd97ae04592
SimeoW <simeonwuthier@gmail.com>
2019-12-07 22:41:06 -0700

    Update histogram generator

--- tools/GetMsgInfoDatasetHistograms.py
+++ tools/GetMsgInfoDatasetHistograms.py
@@ -1,20 +1,24 @@
 import os
 import csv
+import time
 
 # Generates the probability of each message type occuring
 
 def main():
 	os.system('clear')
+	timeStart = time.perf_counter()
 	file = open(os.path.expanduser(f'~/Desktop/GetMsgInfoDatasetProbabilities.csv'), 'w+')
 	file.write(getHeader())
 	for fileName in os.listdir('GetMsgInfoDatasets'):
 		if fileName.endswith(".csv"):
 			file.write(parseFile(fileName, f'GetMsgInfoDatasets/{fileName}'))
+	timeEnd = time.perf_counter()
 	print('Successfully generated "GetMsgInfoDatasetProbabilities.csv" on the Desktop.')
+	print(f'\nThat took exactly {timeEnd - timeStart} seconds.')
 
 def parseFile(fileName, path):
 	print(f'Parsing "{fileName}"...')
-	histogram = probability = {
+	histogram = bytesHistogram = clocksHistogram = {
 		'VERSION': 0,
 		'VERACK': 0,
 		'ADDR': 0,
@@ -74,20 +78,80 @@ def parseFile(fileName, path):
 				histogram['BLOCKTXN'] += max(0, int(row[244]))
 				histogram['REJECT'] += max(0, int(row[254]))
 
+				clocksHistogram['VERSION'] += max(0, int(row[5]))
+				clocksHistogram['VERACK'] += max(0, int(row[15]))
+				clocksHistogram['ADDR'] += max(0, int(row[25]))
+				clocksHistogram['INV'] += max(0, int(row[35]))
+				clocksHistogram['GETDATA'] += max(0, int(row[45]))
+				clocksHistogram['MERKLEBLOCK'] += max(0, int(row[55]))
+				clocksHistogram['GETBLOCKS'] += max(0, int(row[65]))
+				clocksHistogram['GETHEADERS'] += max(0, int(row[75]))
+				clocksHistogram['TX'] += max(0, int(row[85]))
+				clocksHistogram['HEADERS'] += max(0, int(row[95]))
+				clocksHistogram['BLOCK'] += max(0, int(row[105]))
+				clocksHistogram['GETADDR'] += max(0, int(row[115]))
+				clocksHistogram['MEMPOOL'] += max(0, int(row[125]))
+				clocksHistogram['PING'] += max(0, int(row[135]))
+				clocksHistogram['PONG'] += max(0, int(row[145]))
+				clocksHistogram['NOTFOUND'] += max(0, int(row[155]))
+				clocksHistogram['FILTERLOAD'] += max(0, int(row[165]))
+				clocksHistogram['FILTERADD'] += max(0, int(row[175]))
+				clocksHistogram['FILTERCLEAR'] += max(0, int(row[185]))
+				clocksHistogram['SENDHEADERS'] += max(0, int(row[195]))
+				clocksHistogram['FEEFILTER'] += max(0, int(row[205]))
+				clocksHistogram['SENDCMPCT'] += max(0, int(row[215]))
+				clocksHistogram['CMPCTBLOCK'] += max(0, int(row[225]))
+				clocksHistogram['GETBLOCKTXN'] += max(0, int(row[235]))
+				clocksHistogram['BLOCKTXN'] += max(0, int(row[245]))
+				clocksHistogram['REJECT'] += max(0, int(row[255]))
+
+				bytesHistogram['VERSION'] += max(0, int(row[9]))
+				bytesHistogram['VERACK'] += max(0, int(row[19]))
+				bytesHistogram['ADDR'] += max(0, int(row[29]))
+				bytesHistogram['INV'] += max(0, int(row[39]))
+				bytesHistogram['GETDATA'] += max(0, int(row[49]))
+				bytesHistogram['MERKLEBLOCK'] += max(0, int(row[59]))
+				bytesHistogram['GETBLOCKS'] += max(0, int(row[69]))
+				bytesHistogram['GETHEADERS'] += max(0, int(row[79]))
+				bytesHistogram['TX'] += max(0, int(row[89]))
+				bytesHistogram['HEADERS'] += max(0, int(row[99]))
+				bytesHistogram['BLOCK'] += max(0, int(row[109]))
+				bytesHistogram['GETADDR'] += max(0, int(row[119]))
+				bytesHistogram['MEMPOOL'] += max(0, int(row[129]))
+				bytesHistogram['PING'] += max(0, int(row[139]))
+				bytesHistogram['PONG'] += max(0, int(row[149]))
+				bytesHistogram['NOTFOUND'] += max(0, int(row[159]))
+				bytesHistogram['FILTERLOAD'] += max(0, int(row[169]))
+				bytesHistogram['FILTERADD'] += max(0, int(row[179]))
+				bytesHistogram['FILTERCLEAR'] += max(0, int(row[189]))
+				bytesHistogram['SENDHEADERS'] += max(0, int(row[199]))
+				bytesHistogram['FEEFILTER'] += max(0, int(row[209]))
+				bytesHistogram['SENDCMPCT'] += max(0, int(row[219]))
+				bytesHistogram['CMPCTBLOCK'] += max(0, int(row[229]))
+				bytesHistogram['GETBLOCKTXN'] += max(0, int(row[239]))
+				bytesHistogram['BLOCKTXN'] += max(0, int(row[249]))
+				bytesHistogram['REJECT'] += max(0, int(row[259]))
+
 				
 			lineCount += 1
 	totalMessages = 0
+	totalBytes = 0
+	totalClocks = 0
 	for messageType in histogram:
 		totalMessages += histogram[messageType]
+		totalBytes += bytesHistogram[messageType]
+		totalClocks += clocksHistogram[messageType]
 
 	line = ''
 	line += fileName + ','
 	line += str(lineCount) + ','
-	for messageType in histogram:
-		line += '"' + str(histogram[messageType] / totalMessages) + '",'
 	line += str(totalMessages) + ','
+	line += str(totalBytes) + ','
+	line += str(totalClocks) + ','
 	for messageType in histogram:
-		line += '"' + str(histogram[messageType]) + '",'
+		line += str(histogram[messageType] / totalMessages) + ','
+		line += str(bytesHistogram[messageType] / totalBytes) + ','
+		line += str(clocksHistogram[messageType] / totalClocks) + ','
 	line += '\n'
 	return line
 
@@ -95,59 +159,114 @@ def getHeader():
 	line = ''
 	line += '"File name",'
 	line += '"Num rows",'
-	line += '"P(VERSION)",'
-	line += '"P(VERACK)",'
-	line += '"P(ADDR)",'
-	line += '"P(INV)",'
-	line += '"P(GETDATA)",'
-	line += '"P(MERKLEBLOCK)",'
-	line += '"P(GETBLOCKS)",'
-	line += '"P(GETHEADERS)",'
-	line += '"P(TX)",'
-	line += '"P(HEADERS)",'
-	line += '"P(BLOCK)",'
-	line += '"P(GETADDR)",'
-	line += '"P(MEMPOOL)",'
-	line += '"P(PING)",'
-	line += '"P(PONG)",'
-	line += '"P(NOTFOUND)",'
-	line += '"P(FILTERLOAD)",'
-	line += '"P(FILTERADD)",'
-	line += '"P(FILTERCLEAR)",'
-	line += '"P(SENDHEADERS)",'
-	line += '"P(FEEFILTER)",'
-	line += '"P(SENDCMPCT)",'
-	line += '"P(CMPCTBLOCK)",'
-	line += '"P(GETBLOCKTXN)",'
-	line += '"P(BLOCKTXN)",'
-	line += '"P(REJECT)",'
 	line += '"Total num messages",'
-	line += '"Num VERSION",'
-	line += '"Num VERACK",'
-	line += '"Num ADDR",'
-	line += '"Num INV",'
-	line += '"Num GETDATA",'
-	line += '"Num MERKLEBLOCK",'
-	line += '"Num GETBLOCKS",'
-	line += '"Num GETHEADERS",'
-	line += '"Num TX",'
-	line += '"Num HEADERS",'
-	line += '"Num BLOCK",'
-	line += '"Num GETADDR",'
-	line += '"Num MEMPOOL",'
-	line += '"Num PING",'
-	line += '"Num PONG",'
-	line += '"Num NOTFOUND",'
-	line += '"Num FILTERLOAD",'
-	line += '"Num FILTERADD",'
-	line += '"Num FILTERCLEAR",'
-	line += '"Num SENDHEADERS",'
-	line += '"Num FEEFILTER",'
-	line += '"Num SENDCMPCT",'
-	line += '"Num CMPCTBLOCK",'
-	line += '"Num GETBLOCKTXN",'
-	line += '"Num BLOCKTXN",'
-	line += '"Num REJECT",'
+	line += '"Total num bytes",'
+	line += '"Total num clocks",'
+
+	line += '"P(VERSION)",'
+	line += '"P(VERSION bytes)",'
+	line += '"P(VERSION clocks)",'
+
+	line += '"P(VERACK)",'
+	line += '"P(VERACK bytes)",'
+	line += '"P(VERACK clocks)",'
+
+	line += '"P(ADDR)",'
+	line += '"P(ADDR bytes)",'
+	line += '"P(ADDR clocks)",'
+
+	line += '"P(INV)",'
+	line += '"P(INV bytes)",'
+	line += '"P(INV clocks)",'
+
+	line += '"P(GETDATA)",'
+	line += '"P(GETDATA bytes)",'
+	line += '"P(GETDATA clocks)",'
+
+	line += '"P(MERKLEBLOCK)",'
+	line += '"P(MERKLEBLOCK bytes)",'
+	line += '"P(MERKLEBLOCK clocks)",'
+
+	line += '"P(GETBLOCKS)",'
+	line += '"P(GETBLOCKS bytes)",'
+	line += '"P(GETBLOCKS clocks)",'
+
+	line += '"P(GETHEADERS)",'
+	line += '"P(GETHEADERS bytes)",'
+	line += '"P(GETHEADERS clocks)",'
+
+	line += '"P(TX)",'
+	line += '"P(TX bytes)",'
+	line += '"P(TX clocks)",'
+
+	line += '"P(HEADERS)",'
+	line += '"P(HEADERS bytes)",'
+	line += '"P(HEADERS clocks)",'
+
+	line += '"P(BLOCK)",'
+	line += '"P(BLOCK bytes)",'
+	line += '"P(BLOCK clocks)",'
+
+	line += '"P(GETADDR)",'
+	line += '"P(GETADDR bytes)",'
+	line += '"P(GETADDR clocks)",'
+
+	line += '"P(MEMPOOL)",'
+	line += '"P(MEMPOOL bytes)",'
+	line += '"P(MEMPOOL clocks)",'
+
+	line += '"P(PING)",'
+	line += '"P(PING bytes)",'
+	line += '"P(PING clocks)",'
+
+	line += '"P(PONG)",'
+	line += '"P(PONG bytes)",'
+	line += '"P(PONG clocks)",'
+
+	line += '"P(NOTFOUND)",'
+	line += '"P(NOTFOUND bytes)",'
+	line += '"P(NOTFOUND clocks)",'
+
+	line += '"P(FILTERLOAD)",'
+	line += '"P(FILTERLOAD bytes)",'
+	line += '"P(FILTERLOAD clocks)",'
+
+	line += '"P(FILTERADD)",'
+	line += '"P(FILTERADD bytes)",'
+	line += '"P(FILTERADD clocks)",'
+
+	line += '"P(FILTERCLEAR)",'
+	line += '"P(FILTERCLEAR bytes)",'
+	line += '"P(FILTERCLEAR clocks)",'
+
+	line += '"P(SENDHEADERS)",'
+	line += '"P(SENDHEADERS bytes)",'
+	line += '"P(SENDHEADERS clocks)",'
+
+	line += '"P(FEEFILTER)",'
+	line += '"P(FEEFILTER bytes)",'
+	line += '"P(FEEFILTER clocks)",'
+
+	line += '"P(SENDCMPCT)",'
+	line += '"P(SENDCMPCT bytes)",'
+	line += '"P(SENDCMPCT clocks)",'
+
+	line += '"P(CMPCTBLOCK)",'
+	line += '"P(CMPCTBLOCK bytes)",'
+	line += '"P(CMPCTBLOCK clocks)",'
+
+	line += '"P(GETBLOCKTXN)",'
+	line += '"P(GETBLOCKTXN bytes)",'
+	line += '"P(GETBLOCKTXN clocks)",'
+
+	line += '"P(BLOCKTXN)",'
+	line += '"P(BLOCKTXN bytes)",'
+	line += '"P(BLOCKTXN clocks)",'
+
+	line += '"P(REJECT)",'
+	line += '"P(REJECT bytes)",'
+	line += '"P(REJECT clocks)",'
+
 	line += '\n'
 	return line
 
